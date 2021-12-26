aa63e12babf5dba30c4ea29d9f70eff587ead8bb
SuuperW <SuuperW@gmail.com>
2021-12-26 03:51:36 -0600

    I forgot to add this file.

---
+++ DeSmuME Movie Editor/DsmVersionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeSmuMe_Movie_Editor
+{
+	public class DsmVersionInfo
+	{
+		public bool is_x64;
+		public string name;
+		public string processName;
+
+		public long currentFramePtr;
+		public long movieRecordsPtr;
+	}
+}
